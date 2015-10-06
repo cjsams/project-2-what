40e35bdb674a7b8330b3f5d08293c7aa33cfc262
cjsams <csams@mail.bradley.edu>
2015-10-06 03:34:00 -0500

    Final
    added color changes and more ways to type same code

--- what.js
+++ what.js
@@ -8,8 +8,16 @@ what = what.replace('?what=', '');
 what = what.replace(/\+/g, ' ');
 what = decodeURIComponent(what);
 textarea.value = what;
-if((textarea.value) == ('up up down down left right left right b a start')){
+if((textarea.value) == ('up up down down left right left right b a start' ) || (textarea.value) == ("^^vv<><>ba") || (textarea.value) == ("^^vv<><>bastart")|| (textarea.value) == ("up up down down left right left right b a")){
   document.write('<img src="http://i.imgur.com/nUjAyVw.jpg" alt="Konami code" height="385" width="616">');
-  document.write('<h2>https://en.wikipedia.org/wiki/List_of_Konami_code_games</h2>');
-  document.write('<h3><a href="https://en.wikipedia.org/wiki/List_of_Konami_code_games"> Compatible Games </a></h3>');
+  document.write('<h2 id="link"><a href="https://en.wikipedia.org/wiki/List_of_Konami_code_games"> List of Compatible Games and Other Things That Use The Code </a></h2>');
+  document.getElementById("what").style.color = "blue";
+
+  document.getElementById("page").style.color = "Red";
+  document.getElementById("page").style.fontSize = "40px";
+  document.getElementById("header").innerHTML = 'Correct!';//http://www.w3schools.com/js/js_htmldom_html.asp
+  document.getElementById("link").style.color = "yellow";
+  document.getElementById("what").style.height = "30px";
+
+
 }
