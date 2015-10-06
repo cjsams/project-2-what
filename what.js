db31d354ab845c7cd32df339b4255171865608cd
cjsams <csams@mail.bradley.edu>
2015-10-06 02:35:12 -0500

    The Code single server site

--- what.js
+++ what.js
@@ -8,3 +8,8 @@ what = what.replace('?what=', '');
 what = what.replace(/\+/g, ' ');
 what = decodeURIComponent(what);
 textarea.value = what;
+if((textarea.value) == ('up up down down left right left right b a start')){
+  document.write('<img src="http://i.imgur.com/nUjAyVw.jpg" alt="Konami code" height="385" width="616">');
+  document.write('<h2>https://en.wikipedia.org/wiki/List_of_Konami_code_games</h2>');
+  document.write('<h3><a href="https://en.wikipedia.org/wiki/List_of_Konami_code_games"> Compatible Games </a></h3>');
+}
